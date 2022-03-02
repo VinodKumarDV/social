8897c8a5f627ba45f8f39db1c06b602f2c62a199
vinodkumarDV <dvvinod143@gmail.com>
2022-03-02 17:12:58 +0530

    updated app file

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-// import { StyleSheet, View } from 'react-native';
-// import Login from './src/components/Login'; //completed
-// import Signup from './src/components/Signup'; //completed
-// import Resetpassword from './src/components/Resetpassword'; //completed
-// import MyProfile from './src/components/MyProfile'; //completed
-// import Profile from './src/components/Profile'; //completed
-// import EditProfile from './src/components/EditProfile';
-// import Connections from './src/components/Connections';
-// import List from './src/components/List';
-// import Messages from './src/components/Messages';
-// import Textmgs from './src/components/Textmgs';
-// import Post from './src/components/Post';
-// import Setings from './src/components/Setings'
+import { StyleSheet, View } from 'react-native';
+import Login from './src/components/Login'; //completed
+import Signup from './src/components/Signup'; //completed
+import Resetpassword from './src/components/Resetpassword'; //completed
+import MyProfile from './src/components/MyProfile'; //completed
+import Profile from './src/components/Profile'; //completed
+import EditProfile from './src/components/EditProfile';
+import Connections from './src/components/Connections';
+import List from './src/components/List';
+import Messages from './src/components/Messages';
+import Textmgs from './src/components/Textmgs';
+import Post from './src/components/Post';
+import Setings from './src/components/Setings'
 
 
 import { StatusBar } from "react-native";
@@ -23,6 +23,7 @@ export default function App() {
     <>
       <StatusBar barStyle="dark-content" hidden={false} backgroundColor="white" translucent={true} />
       <TabNavigations />
+      {/* <Login/> */}
     </>
   );
 };
