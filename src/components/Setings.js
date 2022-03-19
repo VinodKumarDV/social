d5c9c710b050b33fadeac6ff607b87a8b5b5d9ac
vinodkumarDV <dvvinod143@gmail.com>
2022-03-19 13:42:56 +0530

    added a refer frd function to settings

--- src/components/Setings.js
+++ src/components/Setings.js
@@ -1,31 +1,50 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { Share, View, Text, Image, StyleSheet } from 'react-native'
 
 const IMAGES = {
     social: require('../../assets/socialimg.png'),
 }
 
 export default function Setings() {
-  return (
-      <View style={{ backgroundColor: 'black', marginTop: 30, height: '100%', width: '100%', alignItems: 'center' }}>
-          <Text style={styles.itemName}>Refer friends</Text>
-          <Text style={styles.itemName}>Allow invitations</Text>
-          <Text style={styles.itemName}>Profile visibility status</Text>
-          <Text style={styles.itemName}>Notifications</Text>
-          <Text style={styles.itemName}>Privacy policy</Text>
-          <Text style={styles.itemName}>Help</Text>
-          <Text style={styles.itemName}>Logout</Text>
+    const onShare = async () => {
+        try {
+            const result = await Share.share({
+                message:
+                    'React Native | A framework for building native apps using React',
+            });
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // shared with activity type of result.activityType
+                } else {
+                    // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // dismissed
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+    return (
+        <View style={{ backgroundColor: 'black', marginTop: 30, height: '100%', width: '100%', alignItems: 'center' }}>
+            <Text onPress={onShare} style={styles.itemName}>Refer friends</Text>
+            <Text style={styles.itemName}>Allow invitations</Text>
+            <Text style={styles.itemName}>Profile visibility status</Text>
+            <Text style={styles.itemName}>Notifications</Text>
+            <Text style={styles.itemName}>Privacy policy</Text>
+            <Text style={styles.itemName}>Help</Text>
+            <Text style={styles.itemName}>Logout</Text>
 
-          <View style={styles.itemContainer}>
-              <Image
-                  style={styles.logo}
-                  source={IMAGES.social}
-              />
-              <Text style={styles.version}>v.1.1.1</Text>
-          </View>
-          
-    </View>
-  )
+            <View style={styles.itemContainer}>
+                <Image
+                    style={styles.logo}
+                    source={IMAGES.social}
+                />
+                <Text style={styles.version}>v.1.1.1</Text>
+            </View>
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
